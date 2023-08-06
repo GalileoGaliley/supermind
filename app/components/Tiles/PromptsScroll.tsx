dadbb426e1046e7e343eef2a86b95c28a9545a4d
Tokan Vlad <vlad.tokan96@mail.ru>
2023-08-07 00:00:56 +0300

    Suggested

--- app/components/Tiles/PromptsScroll.tsx
+++ app/components/Tiles/PromptsScroll.tsx
@@ -2,9 +2,25 @@ import React from 'react';
 import {ScrollView, StyleSheet} from 'react-native';
 import PromptTile from './PromptTile';
 import {ChatPromptItem} from '../../store/chatsPromo/chatsPromo.types';
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamsList } from "../../navigation/types";
 
 type OwnProps = ChatPromptItem;
 const PromptsScroll = ({chatsPrompt, id}: OwnProps) => {
+  const navigation = useNavigation<StackNavigationProp<RootStackParamsList>>();
+  const onClick = (prompt: string) => {
+    navigation.navigate('ChatNavigation', {
+      screen: 'ChatScreen',
+      params: {
+        data: {
+          id: 0,
+          messages: [{content: prompt, role: 'user'}],
+          createdAt: '',
+        },
+      },
+    });
+  };
 
   return (
     <ScrollView
@@ -15,6 +31,7 @@ const PromptsScroll = ({chatsPrompt, id}: OwnProps) => {
         return (
           <PromptTile
             id={id}
+            onClick={() => onClick(item.prompt)}
             title={item.title}
             desc={item.desc}
             prompt={item.prompt}
